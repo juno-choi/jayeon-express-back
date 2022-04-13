cffca00f2f11de77ca8ee19ff81f19f15cec8001
juno-choi <ililil9482@naver.com>
2022-04-13 17:00:09 +0900

    FIX : 회원가입 로직 수정 및 url env 설정 추가

--- router/auth/auth.js
+++ router/auth/auth.js
@@ -1,19 +1,22 @@
 const router = require('express').Router();
 const axios = require('axios');
-const url = process.env.API_URL;
-
 require('dotenv').config()
 
+const url = process.env.API_URL;
+
+
+//회원가입
 router.post('/join', async (req, res)=>{
     console.info('회원가입 요청 시작');
     const body = req.body;
-    await axios.post('http://api.jayeonapple.com/login-service/game/join',body)
+    await axios.post(url + '/login-service/game/join',body)
     .then(response => {
         console.info('회원가입 요청 정상 종료');
         res.send(response.data);
     })
     .catch(error => {
         console.error('회원가입 요청 실패');
+        console.error(error.response.data);
         res.send(error.response.data);
     })
     
--- server.js
+++ server.js
@@ -50,5 +50,5 @@ app.post('/api/user', (req,res)=>{
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`server on port ${port}`)
 })
